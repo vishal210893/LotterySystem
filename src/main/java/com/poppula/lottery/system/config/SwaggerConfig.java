053841476d92d597226810d8fda66e0eac072199
Vishal Kumar <vishal210893@gmail.com>
2024-09-09 23:15:11 +0530

    swagger config added

---
+++ src/main/java/com/poppula/lottery/system/config/SwaggerConfig.java
@@ -0,0 +1,26 @@
+package com.poppula.lottery.system.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI springShopOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Lottery API")
+                        .description("API documentation for the Lottery application")
+                        .version("v1.0")
+                        .license(new License().name("Apache 2.0")
+                                .url("http://springdoc.org"))
+                        .contact(new Contact().name("Vishal Kumar")
+                                .url("https://github.com/vishal210893")
+                                .email("vishal210893@gmail.com")));
+    }
+
+}
